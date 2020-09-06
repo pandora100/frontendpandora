6b578fa80198bf93aca44ab40c4a27d88698b2d0
moncho01 <rramossh@hotmail.com>
2020-09-06 01:50:17 -0400

    subida demo 2

--- webpack.config.js
+++ webpack.config.js
@@ -13,24 +13,56 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          options: {
+           plugins: [require('babel-plugin-transform-object-rest-spread')]
+        },
         }
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader"
+            loader: "style-loader",
+            test: /\.css$/
           },
           {
             loader: "css-loader",
-            options: {
+            test: /\.css$/
+             options: {
               modules: true,
               importLoaders: 1,
               localIdentName: "[name]_[local]_[hash:base64]",
               sourceMap: true,
               minimize: true
             }
+          },
+          {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              query: {
+                name:'assets/[name].[ext]'
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              query: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                gifsicle: {
+                  interlaced: true,
+                },
+                optipng: {
+                  optimizationLevel: 7,
+                }
+              }
+            }   
           }
         ]
       }
