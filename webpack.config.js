bdbd4a618ec61635be7dbc127dad1d322750b495
moncho01 <rramossh@hotmail.com>
2020-09-06 20:07:39 -0400

    adecuacion demo 6

--- webpack.config.js
+++ webpack.config.js
@@ -28,24 +28,12 @@ module.exports = {
           loader: "babel-loader"
         }
       },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
-            }
-          }
+       {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
         ]
       }
-    ]
-  }
+    
+ 
 };
